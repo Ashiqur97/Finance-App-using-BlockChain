774a4047249dc8367a1d168752a7519133af0090
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2025-09-15 16:01:58 +0600

    solidity code updating

--- contracts/Finance.sol
+++ contracts/Finance.sol
@@ -86,8 +86,54 @@ contract Loans is FinanceCore {
 
     function getLoans() external view returns (Loan[] memory) {
         return loans[msg.sender];
+    } 
+}
+
+contract Investments is FinanceCore {
+
+    struct Investment {
+        address investor;
+        uint256 amount;
+        string investmentType;
+        uint256 startTime;
+        bool active;
     }
 
-    
+    mapping (address => Investment[]) public investments;
+
+    event InvestmentMade(address indexed investor, uint256 amount, string investmentType);
+    event InvestmentWithdrawn(address indexed investor, uint256 amount, string investmentType);
+
+        function makeInvestment(string memory investmentType) external payable {
+        require(msg.value > 0, ">0");
+        
+        investments[msg.sender].push(Investment({
+            investor: msg.sender,
+            amount: msg.value,
+            investmentType: investmentType,
+            startTime: block.timestamp,
+            active: true
+        }));
+        
+        emit InvestmentMade(msg.sender, msg.value, investmentType);
+    }
+
+        function withdrawInvestment(uint256 investmentIndex) external nonReentrant {
+        require(investmentIndex < investments[msg.sender].length, "Invalid");
+        Investment storage investment = investments[msg.sender][investmentIndex];
+        require(investment.active, "Not active");
+        
+        uint256 returnAmount = investment.amount * 110 / 100; // 10% return
+        investment.active = false;
+        balances[msg.sender] += returnAmount;
+        
+        emit InvestmentWithdrawn(msg.sender, returnAmount, investment.investmentType);
+    }
+
+        function getInvestments() external view returns (Investment[] memory) {
+        return investments[msg.sender];
+    }
 }
+
+
 
